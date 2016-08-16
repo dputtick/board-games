02a899f5bc039fe1509fdf2cc6261c7740c6db3f
Dan Puttick <dputtick@users.noreply.github.com>
2016-08-15 22:31:53 -0400

    first battleship commit

---
+++ battleship/battleship.py
@@ -0,0 +1,25 @@
+class Game():
+    def __init__(self):
+        # objects we need: board object
+        # methods we need: moves, checking for 
+
+
+    class Board():
+        def __init__(self):
+            row = ['*' for _ in range(10)]
+            self.matrix = [list(row) for _ in range(10)]
+            # is this the best way to make a matrix efficiently?
+            # should I use an @property here for the board?
+            
+        def 
+
+
+
+
+def main():
+    # instantiate game object
+    pass
+
+
+if __name__ == '__main__':
+    main()
