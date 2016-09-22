17c399072540194134f1cba102f7fa911e945647
Dan Puttick <dputtick@users.noreply.github.com>
2016-09-21 22:08:18 -0400

    split place_ship tests

--- battleship/test_battleship.py
+++ battleship/test_battleship.py
@@ -11,7 +11,17 @@ class TestBoard:
     def test_init(self, board):
         assert len(board._matrix) == 4
 
-    def test_place(self, board):
-        board.place_ship((0,0), 2, 'v')
+    def test_matrix_updates_on_place_ship(self, board):
+        add_ship_origin_vertical(board)
         assert board._matrix[0][0] == 'x'
-        assert board._ships[-1]['location'] == (0,0)
+
+    def test_ships_update_on_place_ship(self, board):
+        add_ship_origin_vertical(board)
+        test_ship = board._ships[-1]
+        assert test_ship['location'] == (0,0)
+        assert test_ship['length'] == 2
+        assert test_ship['direction'] == 'v'
+
+
+def add_ship_origin_vertical(board):
+    board.place_ship((0,0), 2, 'v')
