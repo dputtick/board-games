aa1ac04866396521a998e9e8e6f9d46a2fb8a441
Dan Puttick <dputtick@users.noreply.github.com>
2016-09-22 16:14:56 +0200

    changed id_name to playerid

--- battleship/battleship.py
+++ battleship/battleship.py
@@ -1,21 +1,24 @@
 class Game():
     def __init__(self):
         # create board (maybe prompt for size)
-        self.board = Board(4)
-        players = []
-        for playerid in (1, 2):
-            players.append(Player(playerid))
+        self.players = []
+        self.boards = {}
+        for playerid in [1]:
+            self.players.append(Player(playerid))
+            self.boards[playerid] = Board(4, playerid)
 
     def run(self):
         self.setup_phase()
+        self.board.render()
 
     def setup_phase(self):
         for player in self.players:
-            ship_lengths = [2, 2]
+            board = boards[player.playerid]
+            ship_lengths = [2]
             ship_id = 1
             while ship_lengths:
                 ship_data = player.get_locations(ship_lengths)
-                if valid_ship(ship_data):
+                if self.valid_ship(ship_data):
                     self.board.place_ship(ship_id, ship_data)
                     ship_lengths.remove(ship_data['length'])
                 else:
@@ -29,32 +32,34 @@ class Game():
 
 
 class Player():
-    def __init__(self, id_name):
+    def __init__(self, playerid):
         self.human = True
-        self.id_name = id_name
+        self.playerid = playerid
 
     def get_move(self):
-        move = input("Player {}, enter your next move".format(self.id_name))
+        move = input("Player {}, enter your next move".format(self.playerid))
         return move
 
     def get_locations(self, ship_lengths):
         print("Remaining ships: ", *ship_lengths)
-        y, x = input("Player {} location:\n".format(self.id_name))
+        loc_strings = input("Player {} location:\n".format(self.playerid))
+        x, y = (int(n) for n in loc_strings.split()) # TODO sanitize input here
         direction = input("Direction:\n")
-        length = input("Length:\n")
+        length = int(input("Length:\n"))
         return {'location': (y, x),
                 'direction': direction,
-                'length'}
+                'length': length}
                 
 
 class Board():
-    def __init__(self, size):
+    def __init__(self, size, player):
+        self.player = player
         row = [0 for _ in range(size)]
         self.matrix = [list(row) for _ in range(size)]
         self.ships = {}
 
     def render(self):
-        for row in self._matrix:
+        for row in self.matrix:
             print(row)
 
 
@@ -66,14 +71,13 @@ class Board():
         for _ in range(ship_data['length']):
             self.matrix[y][x] = ship_id
             if ship_data['direction'] == 'v':
-                y -= 1
+                y += 1
             else:
-                x -= 1
+                x += 1
 
 
 def main():
-    board_size = 4
-    game = Game(board_size)
+    game = Game()
     game.run()
 
 
